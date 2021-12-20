98c71b65c79e9ec678d3e1f30a362b3ae8bef2f3
xzg5101 <xzg5101@psu.edu>
2021-12-19 19:43:56 -0500

    feat: basic setup and home page initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./resources/images/logo640.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>Well come to ME Design.</p>
+        <p className="App-tempLang">
+          The website development is still in progress, see you soon~
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
